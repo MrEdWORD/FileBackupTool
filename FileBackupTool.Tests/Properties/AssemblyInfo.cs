b5adf86573318925e7ddf7d20068c6d1b1262de9
Eddy <eddyharrington@gmail.com>
2018-04-25 17:56:47 -0700

    Added backup settings and test
    Added classes and project files to store backup settings and to test them

---
+++ FileBackupTool.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileBackupTool.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileBackupTool.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1cc11adf-1d50-4d15-922f-1f17fceb4424")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- FileBackupTool/BackupSettings.cs
+++ FileBackupTool/BackupSettings.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FileBackupTool
 {
-    class BackupSettings
+    public class BackupSettings
     {
         private string Name { get; set; }
         private string Description { get; set; }
@@ -19,6 +20,26 @@ namespace FileBackupTool
             Description = description;
             SourceDirectory = sourceDirectory;
             DestinationDirectory = destinationDirectory;
+        }
+
+        public bool ValidateFolderPaths(string folderPath)
+        {
+            //Don't allow null or empty strings as folder paths
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                throw new Exception("The folder path canont be null or empty");
+                //return false;
+            }
+            //Don't allow non-existant folder paths
+            else if (!Directory.Exists(folderPath))
+            {
+                throw new Exception("The folder path does not exist");
+                //return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
