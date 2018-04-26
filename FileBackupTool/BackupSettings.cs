16c85721eeadab25d958d4ef7a06391cbbdc4b2a
Eddy <eddyharrington@gmail.com>
2018-04-25 17:25:41 -0700

    Added backup settings class
    For storing the backup name, description, and directory information

---
+++ FileBackupTool/BackupSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBackupTool
+{
+    class BackupSettings
+    {
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private string SourceDirectory { get; set; }
+        private string DestinationDirectory { get; set; }
+
+        public BackupSettings (string name, string description, string sourceDirectory, string destinationDirectory)
+        {
+            Name = name;
+            Description = description;
+            SourceDirectory = sourceDirectory;
+            DestinationDirectory = destinationDirectory;
+        }
+    }
+}
